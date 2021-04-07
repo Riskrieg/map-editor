2dfb0f37dcda70a9b13d56b309916e1dde57befe
Aaron Yoder <aaronjyoder@gmail.com>
2021-04-06 21:39:08 -0400

    Add mouse pointer variable

--- src/main/kotlin/com/riskrieg/mapeditor/ui/Editor.kt
+++ src/main/kotlin/com/riskrieg/mapeditor/ui/Editor.kt
@@ -1,29 +1,32 @@
 package com.riskrieg.mapeditor.ui
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.riskrieg.mapeditor.Constants
+import java.awt.Point
 
 class Editor(val mapName: String = "") {
 
     private lateinit var base: ImageBitmap
     private lateinit var text: ImageBitmap
 
+    private var mousePos = Point(0, 0)
+
     @Composable
     private fun loadImages() {
-//        base = imageResource(Constants.MAP_PATH + "north-america/north-america-base.png")
-//        text = imageResource(Constants.MAP_PATH + "north-america/north-america-text.png")
+        base = imageResource(Constants.MAP_PATH + "north-america/north-america-base.png")
+        text = imageResource(Constants.MAP_PATH + "north-america/north-america-text.png")
     }
 
     @Composable
@@ -44,12 +47,21 @@ class Editor(val mapName: String = "") {
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     private fun MapView() {
-        Box(modifier = Modifier.combinedClickable(
-            onClick = {
+        Box {
+            Image(bitmap = base, contentDescription = "", modifier = mapModifier())
+            Image(bitmap = text, contentDescription = "", modifier = Modifier.size(text.width.dp, text.height.dp))
+        }
+    }
 
+    @OptIn(ExperimentalFoundationApi::class)
+    private fun mapModifier(): Modifier {
+        return Modifier.size(base.width.dp, base.height.dp).combinedClickable(
+            interactionSource = MutableInteractionSource(),
+            indication = null,
+            onClick = {
+                println("${mousePos.x}, ${mousePos.y}")
             },
             onDoubleClick = {
 
@@ -57,10 +69,10 @@ class Editor(val mapName: String = "") {
             onLongClick = {
 
             }
-        ).indication(interactionSource = MutableInteractionSource() ,indication = null)) {
-//            Image(bitmap = base, contentDescription = "", modifier = Modifier.size(base.width.dp, base.height.dp))
-//            Image(bitmap = text, contentDescription = "", modifier = Modifier.size(text.width.dp, text.height.dp))
-        }
+        ).pointerMoveFilter(onMove = {
+            mousePos = Point(it.x.toInt(), it.y.toInt())
+            false
+        })
     }
 
 }
