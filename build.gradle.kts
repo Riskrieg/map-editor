7df2a43c4f29575595b49e45a95b2af31bbb157f
Aaron Yoder <aaronjyoder@gmail.com>
2021-05-06 04:59:56 -0400

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.riskrieg"
-version = "2.2.4"
+version = "2.2.5"
 
 repositories {
     jcenter()
@@ -48,7 +48,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Exe, TargetFormat.Deb, TargetFormat.Rpm)
             packageName = "Riskrieg Map Editor"
-            packageVersion = "2.2.4"
+            packageVersion = "2.2.5"
             description = "A map editor for Riskrieg."
 
             val iconsRoot = project.file("src/main/resources/icon/")
