d284a390f9798ccc82b2e1aa7dbe54177f657ff8
Aaron J Yoder <aaronjyoder@gmail.com>
2021-10-02 07:08:03 -0400

    Add basic zooming

--- src/main/kotlin/com/riskrieg/editor/ui/component/MapView.kt
+++ src/main/kotlin/com/riskrieg/editor/ui/component/MapView.kt
@@ -12,12 +12,20 @@ import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.mouse.MouseScrollOrientation
+import androidx.compose.ui.input.mouse.MouseScrollUnit
+import androidx.compose.ui.input.mouse.mouseScrollFilter
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.unit.dp
 import com.riskrieg.editor.model.EditorModel
 import java.awt.Point
+import kotlin.math.pow
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, androidx.compose.ui.ExperimentalComposeUiApi::class)
 @Composable
 fun MapView(model: EditorModel, mapViewModifier: Modifier) {
     val stateVertical = rememberScrollState(0)
@@ -25,13 +33,17 @@ fun MapView(model: EditorModel, mapViewModifier: Modifier) {
 
     var pointerPos: Offset by remember { mutableStateOf(Offset(0f, 0f)) }
 
-    val scale: Float by remember { mutableStateOf(1.0f) }
+    var scale: Float by remember { mutableStateOf(1.0f) }
+
+    var canZoom: Boolean by remember { mutableStateOf(false) }
+
+    val maxScale = 1.75f
+    val minScale = 0.65f
 
     val focusRequester = remember(::FocusRequester)
     LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
+        focusRequester.requestFocus() // TODO: Fix focus stuff after entering territory name
     }
-
 
     Box(modifier = mapViewModifier.background(color = Color(255, 255, 255))) {
         Box(
@@ -40,10 +52,10 @@ fun MapView(model: EditorModel, mapViewModifier: Modifier) {
                 .verticalScroll(stateVertical)
                 .horizontalScroll(stateHorizontal)
         ) {
-            Canvas(modifier = Modifier.width((model.mapImage().width * scale).dp)
-                .height((model.mapImage().height * scale).dp).align(
-                    Alignment.Center
-                )
+            Canvas(modifier = Modifier
+                .width((model.mapImage().width * scale).dp)
+                .height((model.mapImage().height * scale).dp)
+                .align(Alignment.Center)
                 .focusable(true)
                 .focusRequester(focusRequester)
                 .focusTarget()
@@ -51,10 +63,29 @@ fun MapView(model: EditorModel, mapViewModifier: Modifier) {
                     onMove = { pointerPos = it; false },
                     onExit = { pointerPos = Offset(0f, 0f); false }
                 )
+                .onKeyEvent { keyEvent ->
+                    canZoom = keyEvent.isCtrlPressed
+                    if (keyEvent.isCtrlPressed && keyEvent.key == Key.Zero) {
+                        scale = 1.0f
+                    }
+                    false
+                }
+                .mouseScrollFilter(onMouseScroll = { event, _ ->
+                    if (event.orientation == MouseScrollOrientation.Vertical && canZoom) {
+                        val deltaY = when (val delta = event.delta) {
+                            is MouseScrollUnit.Line -> -delta.value
+                            is MouseScrollUnit.Page -> -delta.value
+                        }
+                        scale *= 1.02.pow(deltaY.toDouble()).toFloat()
+                        scale = minScale.coerceAtLeast(maxScale.coerceAtMost(scale))
+                    }
+                    canZoom // If false, scroll is enabled, if true, scroll is disabled.
+                })
                 .combinedClickable(
                     indication = null,
                     interactionSource = remember { MutableInteractionSource() },
                     onClick = {
+                        focusRequester.requestFocus()
                         model.interact()
                     }
                 )
@@ -63,6 +94,7 @@ fun MapView(model: EditorModel, mapViewModifier: Modifier) {
                     model.mousePos = Point((pointerPos.x / scale).toInt(), (pointerPos.y / scale).toInt())
 
                     canvas.scale(scale, scale, 0f, 0f)
+
                     canvas.drawImageRect(
                         image = model.mapImage().toComposeBitmap(),
                         paint = Paint().apply { filterQuality = FilterQuality.High })
