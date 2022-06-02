aa1996f974aa1c6e8bccde3a09ba84f8ec16c56f
Aaron J Yoder <aaronjyoder@gmail.com>
2022-06-02 02:33:44 -0400

    Add hover highlight color

--- src/main/kotlin/com/riskrieg/editor/view/EditorView.kt
+++ src/main/kotlin/com/riskrieg/editor/view/EditorView.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import com.riskrieg.editor.view.map.MapView
@@ -43,7 +42,7 @@ class EditorView(val model: EditorViewModel) {
     @Composable
     fun DefaultEditorView() {
         Box(
-            modifier = Modifier.fillMaxSize().background(color = if (model.isDragAndDropping) Color(0, 0, 0, 0xB4) else ViewConstants.UI_BACKGROUND_DARK)
+            modifier = Modifier.fillMaxSize().background(color = if (model.isDragAndDropping) ViewConstants.UI_HOVER_HIGHLIGHT_DARK else ViewConstants.UI_BACKGROUND_DARK)
         ) {
             Text(
                 "Drag a map file (.rkm) or palette file (.rkp) into this window in order to get started.\n\nYou can also go to File -> Import to get started.",
--- src/main/kotlin/com/riskrieg/editor/view/ViewConstants.kt
+++ src/main/kotlin/com/riskrieg/editor/view/ViewConstants.kt
@@ -8,6 +8,9 @@ object ViewConstants {
     var UI_BACKGROUND_DARK = Color(48, 48, 48)
     var UI_BACKGROUND_LIGHT = Color(255, 255, 255)
 
+    var UI_HOVER_HIGHLIGHT_DARK = Color(20, 20, 20, 0xB4)
+    var UI_HOVER_HIGHLIGHT_LIGHT = Color(0, 0, 0, 0xB4)
+
     var UI_FOOTER_DARK = Color(32, 32, 32)
     var UI_FOOTER_LIGHT = Color(255, 255, 255)
 
