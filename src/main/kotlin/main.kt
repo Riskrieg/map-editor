406c90a33cce3b4531fbf2bb7a61c6c8f30fca49
Aaron J Yoder <aaronjyoder@gmail.com>
2022-06-02 02:20:45 -0400

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,3 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
